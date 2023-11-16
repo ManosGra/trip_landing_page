61efeb3e886fcb5040aeeee0d597bdc55221ed74
ManosGra <manosgrammos9@gmail.com>
2023-11-16 20:40:30 +0200

    update

--- hero/hero.php
+++ hero/hero.php
@@ -1,7 +1,7 @@
 <div class="hero">
     <div class="hero-content">
         <div class="container-lg w-100 h-100 text-center">
-            <h1 class="text-uppercase text-white d-flex flex-column justify-content-center h-100  box show">Cabrio day trip to Temple of Poseidon</h1>
+            <h1 class="text-uppercase text-white d-flex flex-column justify-content-center h-100  box show">Cabrio day trip to Athens Riviera</h1>
         </div>
     </div>
 </div>
