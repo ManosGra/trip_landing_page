17affcfe836573c86785d3cda52afececce4e6e9
ManosGra <manosgrammos9@gmail.com>
2023-11-23 17:40:52 +0200

    update

--- content-end/content-end.php
+++ content-end/content-end.php
@@ -1,8 +1,8 @@
 <div class="content-end box show">
     <div class="container-lg">
         <hr>
-        <h5 class="f-bold mb-4" id="included">What's included</h5>
-        <ul class="d-flex flex-row align-items-center p-0 m-0">
+        <h2 class="text-center mb-4 f-bold text-center" id="included">What's included</h2>
+        <ul class="d-flex flex-row align-items-center p-0 m-0 justify-content-center">
             <li class="items list-unstyled d-flex flex-row align-items-center me-4">
                 <?php include 'svgicons/check_svg.php' ?>Coffee/tea,water
             </li>
@@ -14,24 +14,24 @@
 
         <hr>
 
-        <h5 class="f-bold mb-4" id="overview">Additional Info</h5>
-        <div class="row items">
+        <h2 class="text-center mb-4 f-bold text-center" id="overview">Additional Info</h2>
+        <div class="row items text-center">
             <div class="col-md-12">
                 <div class="items">
                     <ul class="mb-0">
-                        <li class="mb-3"> Monday - Sunday : 09:00 AM - 08:00 PM</li>
-                        <li class="mb-3">Confirmation will be received at time of booking</li>
-                        <li class="mb-3">This is a private tour/activity. Only your group will participate</li>
-                        <li class="mb-3">Near public transportation</li>
+                        <li class="mb-3  list-unstyled"> Monday - Sunday : 09:00 AM - 08:00 PM</li>
+                        <li class="mb-3 list-unstyled">Confirmation will be received at time of booking</li>
+                        <li class="mb-3 list-unstyled">This is a private tour/activity. Only your group will participate</li>
+                        <li class="mb-3 list-unstyled">Near public transportation</li>
                     </ul>
                 </div>
             </div>
 
             <div class="col-md-12">
                 <ul>
-                    <li class="mb-3">Most travelers can participate</li>
-                    <li class="mb-3">Not wheelchair accessible</li>
-                    <li class="mb-3">Service animals allowed</li>
+                    <li class="mb-3 list-unstyled">Most travelers can participate</li>
+                    <li class="mb-3 list-unstyled">Not wheelchair accessible</li>
+                    <li class="mb-3 list-unstyled">Service animals allowed</li>
                 </ul>
             </div>
         </div>
