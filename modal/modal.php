cb4aa728dd6a38fd24508e38e214f973a09a4ad6
ManosGra <manosgrammos9@gmail.com>
2023-11-24 21:27:52 +0200

    update

--- modal/modal.php
+++ modal/modal.php
@@ -6,17 +6,20 @@
                 <div class="input-field field">
                     <p class="text-white mb-1 mt-3 f-bold">YOUR NAME*</p>
                     <input type="text" id="name" class="item w-100" autocomplete="off">
+                    <div class="error-txt">Name can't be blank</div>
                 </div>
 
                 <div class="input-field field">
                     <p class="text-white mb-1 mt-4 f-bold">YOUR EMAIL*</p>
                     <input type="text" id="email" class="item w-100" autocomplete="off">
+                    <div class="error-txt">Email can't be blank</div>
                 </div>
             </div>
 
             <div class="textarea-field field">
                 <p class="text-white mb-1 mt-4 f-bold">SUBJECT*</p>
                 <textarea name="" id="message" class="w-100" cols="30" rows="10"></textarea>
+                <div class="error-txt">Subject can't be blank</div>
             </div>
 
             <div class="d-flex flex-row justify-content-between align-items-center">
