9aa12862e7298e1ea228317e338ceffc20496c36
ManosGra <manosgrammos9@gmail.com>
2023-12-01 06:07:27 +0200

    update

--- privacy.php
+++ privacy.php
@@ -1,8 +1,39 @@
 <?php include 'head/head.php' ?>
 
+<header class="header m-0 py-4 w-100">
+    <div class="container-lg">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-6">
+                <div class="logo">
+                    <a href="/#index.php" class="m-0 text-decoration-none text-white">Logo</a>
+                </div>
+            </div>
+
+            <div class="col-6">
+                <ul class="p-0 m-0 text-white d-flex flex-row align-items-center justify-content-between">
+
+                    <li class="p-0 list-unstyled"><a href="/#overview"
+                            class="text-decoration-none text-white">Overview</a>
+                    </li>
+
+                    <li class="p-0 list-unstyled"><a href="/#expect" class="text-decoration-none text-white">What to
+                            expect</a>
+                    </li>
+
+                    <li class="p-0 m-0 list-unstyled"><a href="/#tour" class="text-decoration-none text-white">The
+                            Trip</a>
+                    </li>
+
+                    <li class="p-0  list-unstyled"><a href="/#included" class="text-decoration-none text-white">What's
+                            included</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</header>
+
 <div class="policy text-white">
     <div class="container-lg">
-        <a href="index.php">BACK</a>
         <h1 class="text-white mb-5">Privacy Policy</h1>
         <p class="mb-1">Company Name: Base Constructions</p>
         <p class="mb-1">Domain: baseconstructions.com</p>
