5b0476b2d4f7de4eae29f0b53c84b9505c52df2b
ManosGra <manosgrammos9@gmail.com>
2023-12-04 19:31:48 +0200

    update

--- header/header.php
+++ header/header.php
@@ -1,14 +1,14 @@
 <header class="header m-0 py-4 w-100">
     <div class="container-lg">
         <div class="row justify-content-center align-items-center">
-            <div class="col-4">
+            <div class="col-4 col-md-2">
                 <div class="logo">
                     <a href="#" class="m-0 text-decoration-none text-white">Logo</a>
                 </div>
             </div>
 
-            <div class="col-6">
-                <ul class="p-0 m-0 text-white d-flex flex-row align-items-center item-list justify-content-between">
+            <div class="col-6 col-md-8">
+                <ul class="p-0 m-0 text-white d-flex flex-row align-items-center justify-content-around">
 
                     <li class=" item-list p-0 list-unstyled"><a href="/#overview"
                             class="text-decoration-none text-white">Overview</a>
@@ -28,7 +28,7 @@
                 </ul>
             </div>
 
-            <div class="col-2 text-end">
+            <div class="col-2 text-end col-md-2">
                 <button id="openModal" class="text-decoration-none button">Contact
                     us</button>
 
